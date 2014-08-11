c6f102869a741096d7a509a4ed5343285a175264
Mark Black <mark.prestwick@gmail.com>
2014-08-11 09:51:35 -0500

    Removed unnecessary components.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,21 +14,7 @@ angular.module('ngResearchApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
-    'ngRoute',
     'ngSanitize',
     'ngTouch'
-])
-    .config(function ($routeProvider) {
-        'use strict';
-        $routeProvider.when('/', {
-            templateUrl: 'views/main.html',
-            controller: 'MainCtrl'
-        })
-            .when('/about', {
-                templateUrl: 'views/about.html',
-                controller: 'AboutCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    });
+]);
+
